27c84e4f5d96d3ac67056b2e09b3646c1d66eec0
Manjotkj <117943601+Manjotkj@users.noreply.github.com>
2022-11-15 15:27:31 -0500

    Update AnswerData.java

--- AnswerData.java
+++ AnswerData.java
@@ -5,4 +5,10 @@ public class AnswerData extends Observer {
   File checkFile; 
   
   //Methods
+  void String takeInput() {
+    Scanner reader = new Scanner(System.in);
+    System.out.println("Type in the file name containing the answer");
+    String answerFile = reader.nextLine();
+    
+  void checkExists() {
   
