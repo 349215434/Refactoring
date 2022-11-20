0a105e66468fb36434192c360ec01c894bb7dcda
Manjotkj <117943601+Manjotkj@users.noreply.github.com>
2022-11-20 18:57:18 -0500

    Update AnswerData.java

--- AnswerData.java
+++ AnswerData.java
@@ -9,6 +9,23 @@ public class AnswerData extends Observer {
     Scanner reader = new Scanner(System.in);
     System.out.println("Type in the file name containing the answer");
     String answerFile = reader.nextLine();
+  }
     
-  void checkExists() {
-  
+  void checkFile() {
+    File checkFile = new File(answerFile);
+    while (!checkFile.exists()) {
+      System.out.println("\nThis file does not exist please try again.");
+      answerFile = reader.nextLine().toLowerCase();
+      checkFile = new File(answerFile);    
+    }
+    if(checkFile.exists()){
+      System.out.println("Finding file for you");
+      System.out.println("Please wait as it loads");
+    }
+    return answerFile;
+  }
+    @Override
+  public void update() {
+    System.out.println("updated answerData"); 
+  }
+}
