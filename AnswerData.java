ce0dcddb327d3c6145e15d2f9eada0258274a6d8
Manjotkj <117943601+Manjotkj@users.noreply.github.com>
2022-11-20 19:07:45 -0500

    Update AnswerData.java

--- AnswerData.java
+++ AnswerData.java
@@ -1,28 +1,37 @@
+import java.util.Scanner;
+import java.io.File;
+
 public class AnswerData extends Observer { 
   
   //Attributes
   String answerFile;
   File checkFile; 
+
+  // Constructor: A method that has the same name as the class
+  public answerData(Subject subject){
+    this.subject = subject;
+    //this.subject.attach(this);
+  }
   
   //Methods
-  void String takeInput() {
+String takeInput() {
     Scanner reader = new Scanner(System.in);
-    System.out.println("Type in the file name containing the answer");
+    System.out.println("Type in the file name containing the answers");
     String answerFile = reader.nextLine();
+    return answerFile;
   }
     
   void checkFile() {
-    File checkFile = new File(answerFile);
+    checkFile = new File(answerFile);
     while (!checkFile.exists()) {
       System.out.println("\nThis file does not exist please try again.");
-      answerFile = reader.nextLine().toLowerCase();
+      takeInput();
       checkFile = new File(answerFile);    
     }
     if(checkFile.exists()){
       System.out.println("Finding file for you");
       System.out.println("Please wait as it loads");
     }
-    return answerFile;
   }
     @Override
   public void update() {
