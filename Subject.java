b03218e3cae4d7404876fe41152d90c5904e20a9
349215434 <116114089+349215434@users.noreply.github.com>
2022-11-22 10:02:16 -0500

    Update Subject.java

--- Subject.java
+++ Subject.java
@@ -1,27 +1,46 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.io.File;
 
 public class Subject {
 	
-   private List<Observer> observers = new ArrayList<Observer>();
-   //private int state;
+   private ArrayList<Observer> observers = new ArrayList<Observer>();
 
-   /*public int getState() {
-      //return state;
+   public void registerObserver(Observer observer){
+      observers.add(observer);		
    }
 
-   public void setState(int state) {
-      //this.state = state;
-      notifyAllObservers();
-   }*/
+  void takeInput() {
+    Scanner reader = new Scanner(System.in);
+    System.out.println("\nType in the student response file name (include the file extension)");
+    String studentResponse = reader.nextLine().toLowerCase();
+    checkExist(studentResponse);
+    notifyAllObservers(studentResponse);
 
-   public void registerStudent(Observer observer){
-      this.observers.add(observer);		
-   }
+  }
   
-  public void notifyAllObservers(){
+  void checkExist(String studentResponse) {
+    File checkFile = new File(studentResponse);
+    while (!checkFile.exists()) {
+     System.out.println("You entered: " + studentResponse);
+     System.out.println("This file does not exist. Please try another file\n");
+     takeInput();
+     checkFile = new File(studentResponse);
+   }
+
+    while (!studentResponse.contains("response")) {
+      System.out.println("You entered: " + studentResponse);
+      System.out.println("Cannot open this file. Please try another file\n");
+      takeInput();
+      checkFile = new File(studentResponse);
+     }
+     System.out.println("You entered: " + studentResponse + "\n");
+  }
+  
+  public void notifyAllObservers(String event){
     for (Observer observer : observers) {
-       observer.update();
+       observer.update(event);
     }
-  } 	
+  }//sends to all, find a way that only the one observer does something w it  	
 }
