16d58e0cdf98e7b761b9000349a7739c4f8305df
Manjotkj <117943601+Manjotkj@users.noreply.github.com>
2022-11-22 22:04:03 -0500

    Update StudentData.java

--- StudentData.java
+++ StudentData.java
@@ -1,36 +1,102 @@
-public class StudentData extends Observer{
+import java.util.Scanner;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileWriter;
+
+public class StudentData implements Observer {
 
   // Properties or Attributes
-  String studentFile;
+  String studentResponse;
   File checkFile;
 
-  // Constructor: A method that hasa the same name as the class
-  /*
-  public Pokemon(String name){
-   this.name = name;
-   this.HP = 10;    // All pokemon will have a default of 10hp
-  }*/
+  //Methods
+  /**
+   * This takes the results of the array and makes it a array
+   * @param String data - takes the String data
+   */
+  void printResults(String data) {
+    int totalLines = countingLines(data);
+    String[][] studentsArray = new String[totalLines-1][];
+     //printing result of the array
+    populateArray(data, studentsArray);
 
-  // Methods
-  void String takeInput() {
-    Scanner reader = new Scanner(System.in);
-    System.out.println("Type in the Student Data file name: ");
-    studentFile = reader.nextLine();
+    // This makes it a 1D array first
+    // this is taking each line of the array at a time
+    for (String[] line : studentsArray) {
+      for (String word : line) {
+        System.out.print(word+ "  ");
+      }
+      System.out.println();
+    }
+  }
+
+  /**
+   * counts the lines in the array
+   * @param String file specifically counts the lines of the file the user wants to open
+   * @return -1
+   */
+  public static int countingLines(String file) {
+    BufferedReader br = null;
+    int count = 0;
+    try {
+      br = new BufferedReader(new FileReader(file));
+      String contentLine = br.readLine();
+      while (contentLine != null) {
+        count++;
+        contentLine = br.readLine();
+      }
+      return count;
+    } catch (IOException e) {
+      e.printStackTrace();
+      return -1;
+    } finally {
+      try {
+        if (br != null) {
+          br.close();
+        }
+      } catch (IOException e) {
+        System.out.println("Error in closing the BufferedReader");
+      }
+    }
   }
   
-  void checkExist() {
-    File checkFile = new File(Studentfile);
-    // if the file does not exist
-    while (!checkFile.exists()) {
-      System.out.println("\nThis file does not exist please try again.");
-      studentFile = reader.nextLine();
-      checkFile = new File(studentFile);    
-    }
-    // if the file exists
-    if(checkFile.exists()){
-      System.out.println("Finding file for you");
-      // prints and returns the student file
-      printResults(studentFile);
-      return studentFile;
+  /**
+   * Populates array aka makes it into a array 
+   * @param String file - the file the user wants to open
+   * @param String[][] data - the data in a 2D array
+   */
+  public static void populateArray(String file, String[][] data) {
+    BufferedReader br = null;
+    int lineNum = 0;
+    //Gets rid of the commas from the data as they are not needed 
+    String delim = ",";
+    try {
+      br = new BufferedReader(new FileReader(file));
+      br.readLine();
+      String contentLine = br.readLine();
+      while (contentLine != null) {
+        data[lineNum] = contentLine.split(delim,-1);
+        lineNum++;
+        contentLine = br.readLine();
+      }
+      //catches any errors
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        if (br != null) {
+          br.close();
+        }
+      } catch (IOException e) {
+        System.out.println("Error in closing the BufferedReader");
+      }
     }
   }
+  
+  //@Override
+  public void update(String file) {
+    System.out.println("updated StudentResponse"); 
+  }
+}
